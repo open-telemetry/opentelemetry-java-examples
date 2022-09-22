ff3ece26e5a5d30b4cb09f89d3ec27fc5455f817
Izabela Kulakowska <ikulakowska@microsoft.com>
2022-09-22 17:50:00 +0200

    Format

--- telemetry-testing/src/main/java/io/opentelemetry/example/telemetry/Controller.java
+++ telemetry-testing/src/main/java/io/opentelemetry/example/telemetry/Controller.java
@@ -1,20 +1,16 @@
 package io.opentelemetry.example.telemetry;
 
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.LongHistogram;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
+import java.util.Random;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
 
 @RestController
 public class Controller {
@@ -43,7 +39,7 @@ public class Controller {
 
   @WithSpan
   private void doWork(int sleepTime) throws InterruptedException {
-      Thread.sleep(sleepTime);
-      LOGGER.info("A sample log message!");
+    Thread.sleep(sleepTime);
+    LOGGER.info("A sample log message!");
   }
 }
