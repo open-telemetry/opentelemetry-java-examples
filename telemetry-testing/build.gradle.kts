237d1fbb0b9944d41c259d6a4671e9b6b35dd0d3
Jason Plumb <jplumb@splunk.com>
2024-01-29 15:04:20 -0800

    remove unnecessary default

--- telemetry-testing/build.gradle.kts
+++ telemetry-testing/build.gradle.kts
@@ -52,8 +52,7 @@ tasks.test {
     // otel.exporter.otlp.protocol - the transport protocol to use on OTLP trace, metric, and log requests
     // otel.metric.export.interval - the interval, in milliseconds, between the start of two export attempts
     // More information: https://github.com/open-telemetry/opentelemetry-java/blob/main/sdk-extensions/autoconfigure/README.md
-    jvmArgs = listOf("-Xmx1G", "-javaagent:${agent.singleFile}",
+    jvmArgs = listOf("-Xmx2G", "-javaagent:${agent.singleFile}",
         "-Dotel.exporter.otlp.endpoint=http://127.0.0.1:4318",
-        "-Dotel.exporter.otlp.protocol=http/protobuf",
         "-Dotel.metric.export.interval=5000")
 }
