0c460abd17ca8706fa42b993f382deb0c8352c58
Pranav Sharma <sharmapranav@google.com>
2024-04-02 22:42:53 +0000

    Add example code

--- resource-detection-gcp/src/main/java/io/opentelemetry/resource/gcp/GCPResourceExample.java
+++ resource-detection-gcp/src/main/java/io/opentelemetry/resource/gcp/GCPResourceExample.java
@@ -1,7 +1,34 @@
 package io.opentelemetry.resource.gcp;
 
+import io.opentelemetry.contrib.gcp.resource.GCPResourceProvider;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.ResourceConfiguration;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+
 public class GCPResourceExample {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    // Get the autoconfigured OpenTelemetry SDK
+    OpenTelemetrySdk openTelemetrySdk =
+        AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk();
+
+    // Shows the resource attributes detected from the environment variables
+    // and system properties.
+    System.out.println("Detecting resource: Environment");
+    Resource autoResource = ResourceConfiguration.createEnvironmentResource();
+    System.out.println(autoResource.getAttributes() + "\n");
+
+    // Shows the resource attributes detected by the GCP Resource Provider
+    System.out.println("Detecting resource: hardcoded");
+    GCPResourceProvider resourceProvider = new GCPResourceProvider();
+    System.out.println(resourceProvider.getAttributes() + "\n");
+
+    // Shows the attributes attached to the Resource that was set for TracerProvider
+    // via the autoconfiguration SPI.
+    // This works similarly for MeterProvider and LoggerProvider.
+    System.out.println("Detecting resource: Autoconfigure");
+    SdkTracerProvider autoConfTracerProvider = openTelemetrySdk.getSdkTracerProvider();
+    System.out.println(autoConfTracerProvider.toString() + "\n");
   }
 }
