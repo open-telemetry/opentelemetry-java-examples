c053c3b3c2e937be9e04ecb3ce47620a9c724e8e
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-04-19 09:30:13 +0200

    configure spring OTLP exporter without using internal classes (#394)

--- spring-native/src/main/java/io/opentelemetry/example/graal/Application.java
+++ spring-native/src/main/java/io/opentelemetry/example/graal/Application.java
@@ -1,27 +1,12 @@
 package io.opentelemetry.example.graal;
 
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
-import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
-import io.opentelemetry.semconv.SemanticAttributes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  public AutoConfigurationCustomizerProvider otelCustomizer() {
-    return p ->
-        p.addSamplerCustomizer(
-            (fallback, c) ->
-                RuleBasedRoutingSampler.builder(SpanKind.SERVER, fallback)
-                    .drop(SemanticAttributes.URL_PATH, "^/actuator")
-                    .build());
   }
 }
