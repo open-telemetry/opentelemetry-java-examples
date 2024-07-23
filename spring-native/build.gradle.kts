6dba44f06b3cb2534d23c67468c8bdec93bd028a
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-07-23 09:48:40 +0200

    simplify BOM imports

--- spring-native/build.gradle.kts
+++ spring-native/build.gradle.kts
@@ -11,7 +11,6 @@ val moduleName by extra { "io.opentelemetry.examples.native" }
 
 dependencies {
     implementation(platform(SpringBootPlugin.BOM_COORDINATES))
-    implementation(platform("io.opentelemetry:opentelemetry-bom:1.40.0"))
     implementation(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom:2.6.0"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
