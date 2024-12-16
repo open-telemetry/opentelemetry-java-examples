e9a828d3ae8dfa8a8796a86e17aa4f9aa100fbf1
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-12-16 14:14:49 +0100

    add example for OTLP logging via stdout and k8s

---
+++ .github/scripts/run-acceptance-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+cd oats/yaml
+go install github.com/onsi/ginkgo/v2/ginkgo@latest
+export TESTCASE_TIMEOUT=5m
+export TESTCASE_BASE_PATH=../..
+ginkgo -r
