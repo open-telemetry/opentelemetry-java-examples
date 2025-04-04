21cb7e79fc226faceb7870e2fc107b7fe81ecc7b
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-04-04 18:40:31 +0200

    update oats

--- .github/scripts/run-oats-tests.sh
+++ .github/scripts/run-oats-tests.sh
@@ -8,5 +8,5 @@ popd
 
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-go install https://github.com/grafana/oats@0.2.0
+go install github.com/grafana/oats@v0.2.0
 oats -timeout 5m -lgtm-version logging-k8s-stdout-otlp-json/
