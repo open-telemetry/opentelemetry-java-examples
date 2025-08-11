db7c69024d64101aa90dbf4b3428cdf6b8986876
Lauri Tulmin <ltulmin@splunk.com>
2025-08-11 18:40:24 +0300

    update shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     id("com.diffplug.spotless")
-    id("com.github.johnrengelman.shadow") apply false
+    id("com.gradleup.shadow") apply false
     id("java-library")
 }
 
 subprojects {
     apply(plugin = "com.diffplug.spotless")
     apply(plugin = "java-library")
-    apply(plugin = "com.github.johnrengelman.shadow")
+    apply(plugin = "com.gradleup.shadow")
 
     group = "io.opentelemetry"
     version = "0.1.0-SNAPSHOT"
