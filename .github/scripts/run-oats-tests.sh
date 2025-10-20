8ea7b790957c6b458893bea4f415c958776b1631
Jay DeLuca <jaydeluca4@gmail.com>
2025-10-20 15:58:18 -0400

    trailing slash

--- .github/scripts/run-oats-tests.sh
+++ .github/scripts/run-oats-tests.sh
@@ -14,4 +14,4 @@ wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 go install github.com/grafana/oats@v0.3.1
 oats -timeout 5m logging-k8s-stdout-otlp-json/
-oats -timeout 5m javaagent-declarative-configuration/oats
+oats -timeout 5m javaagent-declarative-configuration/oats/
