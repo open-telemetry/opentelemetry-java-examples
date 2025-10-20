1335eed5bbe69312cddc3c9fb958f6bb340c6fc2
Jay DeLuca <jaydeluca4@gmail.com>
2025-10-20 08:36:21 -0400

    cleanup gradle

--- javaagent-declarative-configuration/build.gradle.kts
+++ javaagent-declarative-configuration/build.gradle.kts
@@ -9,20 +9,13 @@ plugins {
 description = "OpenTelemetry Java Agent Declarative Configuration Example"
 val moduleName by extra { "io.opentelemetry.examples.javaagent.declarative" }
 
-val extension = configurations.create("extension")
-
 dependencies {
     implementation(platform(SpringBootPlugin.BOM_COORDINATES))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    
-    extension("io.opentelemetry.contrib:opentelemetry-samplers:1.50.0-alpha") {
-        isTransitive = false
-    }
 }
 
 val copyExtension = tasks.register<Copy>("copyExtension") {
-    from(extension.singleFile)
     into(layout.buildDirectory.dir("agent"))
     rename(".*\\.jar", "opentelemetry-javaagent-extension.jar")
 }
