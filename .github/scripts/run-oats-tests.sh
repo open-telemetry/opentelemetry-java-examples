edd9e5421612857700b1132fe35b30218f650e27
Jay DeLuca <jaydeluca4@gmail.com>
2025-10-20 15:09:34 -0400

    build in script:

--- .github/scripts/run-oats-tests.sh
+++ .github/scripts/run-oats-tests.sh
@@ -6,6 +6,10 @@ pushd logging-k8s-stdout-otlp-json
 ../gradlew assemble
 popd
 
+pushd javaagent-declarative-configuration
+../gradlew bootJar
+popd
+
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 go install github.com/grafana/oats@v0.3.1
