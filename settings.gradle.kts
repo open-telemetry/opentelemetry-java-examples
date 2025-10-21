50ec854f2743813c81196d6466548593d3808584
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-10-21 13:26:45 -0700

    Enable Develocity build scans (#866)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,46 @@ plugins {
     id("com.gradle.develocity")
 }
 
+val develocityServer = "https://develocity.opentelemetry.io"
+val isCI = System.getenv("CI") != null
+val develocityAccessKey = System.getenv("DEVELOCITY_ACCESS_KEY") ?: ""
+
+// if develocity access key is not given and we are in CI, then we publish to scans.gradle.com
+val useScansGradleCom = isCI && develocityAccessKey.isEmpty()
+
 develocity {
+    if (useScansGradleCom) {
+        buildScan {
+            termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+            termsOfUseAgree = "yes"
+        }
+    } else {
+        server = develocityServer
+        buildScan {
+            publishing.onlyIf { it.isAuthenticated }
+        }
+    }
+
     buildScan {
-        publishing.onlyIf { System.getenv("CI") != null }
-        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
-        termsOfUseAgree.set("yes")
+        uploadInBackground = !isCI
+
+        capture {
+            fileFingerprints = true
+        }
+
+        buildScanPublished {
+            java.io.File("build-scan.txt").printWriter().use { writer ->
+                writer.println(buildScanUri)
+            }
+        }
+    }
+}
+
+if (!useScansGradleCom) {
+    buildCache {
+        remote(develocity.buildCache) {
+            isPush = isCI && develocityAccessKey.isNotEmpty()
+        }
     }
 }
 
