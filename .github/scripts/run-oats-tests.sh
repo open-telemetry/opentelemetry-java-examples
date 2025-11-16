dc7a4042fd19159a0f4ed22a7a58e4eacc769d11
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-11-16 18:01:09 -0500

    Pin k3d dependency (#890)

--- .github/scripts/run-oats-tests.sh
+++ .github/scripts/run-oats-tests.sh
@@ -10,8 +10,12 @@ pushd javaagent-declarative-configuration
 ../gradlew bootJar
 popd
 
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+# renovate: datasource=github-releases depName=k3d-io/k3d
+K3D_VERSION=v5.8.3
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/${K3D_VERSION}/install.sh | TAG=${K3D_VERSION} bash
 
-go install github.com/grafana/oats@v0.3.1
+# renovate: datasource=go depName=github.com/grafana/oats
+OATS_VERSION=v0.3.1
+go install github.com/grafana/oats@${OATS_VERSION}
 oats -timeout 5m logging-k8s-stdout-otlp-json/
 oats -timeout 5m javaagent-declarative-configuration/oats/
