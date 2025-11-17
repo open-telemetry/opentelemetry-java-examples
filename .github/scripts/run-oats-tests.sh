d28ff5761e1b4fa5fbd516a55d8ce2258c1c7ccd
Trask Stalnaker <trask.stalnaker@gmail.com>
2025-11-16 16:03:52 -0800

    Better k3d installation

--- .github/scripts/run-oats-tests.sh
+++ .github/scripts/run-oats-tests.sh
@@ -10,15 +10,10 @@ pushd javaagent-declarative-configuration
 ../gradlew bootJar
 popd
 
-./gradlew :doc-snippets:extensions-testapp:jar :doc-snippets:extensions-minimal:shadowJar
-
-# renovate: datasource=github-releases depName=k3d-io/k3d
-K3D_VERSION=v5.8.3
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/${K3D_VERSION}/install.sh | TAG=${K3D_VERSION} bash
+mise install k3d
 
 # renovate: datasource=go depName=github.com/grafana/oats
 OATS_VERSION=v0.4.1
 go install github.com/grafana/oats@${OATS_VERSION}
 oats -timeout 5m logging-k8s-stdout-otlp-json/
 oats -timeout 5m javaagent-declarative-configuration/oats/
-oats -timeout 5m doc-snippets/extensions-minimal/oats/
