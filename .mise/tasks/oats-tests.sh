5497ab1aff89f3b61c2626dafac6954afd1d97b9
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-11-17 10:55:42 +0100

    add ability to run oats-tests locally

--- .mise/tasks/oats-tests.sh
+++ .mise/tasks/oats-tests.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
+#MISE description="Run OATS tests for example projects"
 
 set -euo pipefail
 
 pushd logging-k8s-stdout-otlp-json
-../gradlew assemble
+../gradlew clean assemble
 popd
 
 pushd javaagent-declarative-configuration
-../gradlew bootJar
+../gradlew clean bootJar
 popd
 
-mise install k3d
-
-# renovate: datasource=go depName=github.com/grafana/oats
-OATS_VERSION=v0.4.1
-go install github.com/grafana/oats@${OATS_VERSION}
 oats -timeout 5m logging-k8s-stdout-otlp-json/
 oats -timeout 5m javaagent-declarative-configuration/oats/
