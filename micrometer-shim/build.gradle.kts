7fb3f8684d83fe458c1209d6e7f407f6eadb22f7
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2025-11-21 09:54:11 +0100

    fix

--- micrometer-shim/build.gradle.kts
+++ micrometer-shim/build.gradle.kts
@@ -31,5 +31,5 @@ dependencies {
     //spring modules
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-aspectj")
 }
