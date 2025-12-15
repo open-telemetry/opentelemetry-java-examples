ad4f0c9f3e7e196f877eb36b1ff06aa3d4f3b629
Jack Berg <34418638+jack-berg@users.noreply.github.com>
2025-12-15 11:04:15 -0600

    Remove temporary opentelemetry-java version pins (#964)

--- doc-snippets/api/build.gradle.kts
+++ doc-snippets/api/build.gradle.kts
@@ -11,8 +11,7 @@ java {
 }
 
 dependencies {
-    // TODO: revert version after 2.23.0 instrumentation release
-    implementation("io.opentelemetry:opentelemetry-api:1.57.0")
+    implementation("io.opentelemetry:opentelemetry-api")
 
     implementation("io.opentelemetry.semconv:opentelemetry-semconv")
     implementation("io.opentelemetry.semconv:opentelemetry-semconv-incubating:1.37.0-alpha")
--- doc-snippets/configuration/build.gradle.kts
+++ doc-snippets/configuration/build.gradle.kts
@@ -11,9 +11,6 @@ java {
 }
 
 dependencies {
-    // TODO: delete after 2.23.0 instrumentation release
-    implementation("io.opentelemetry:opentelemetry-api:1.57.0")
-
     implementation("io.opentelemetry:opentelemetry-sdk")
     implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
 
