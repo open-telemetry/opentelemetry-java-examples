c73b9d96c13550dc7382c8a5fc739424b7ab491c
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-06-25 13:52:40 +0000

    ci: run current OATS consumer suites Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- .mise/tasks/oats-tests.sh
+++ .mise/tasks/oats-tests.sh
@@ -10,7 +10,18 @@ set -euo pipefail
   :opentelemetry-examples-logging-k8s-stdout-otlp-json:assemble \
   :opentelemetry-examples-spring-declarative-configuration:bootJar
 
-oats -timeout 5m doc-snippets/extensions-minimal/oats/
-oats -timeout 5m javaagent-declarative-configuration/oats/
-oats -timeout 5m logging-k8s-stdout-otlp-json/
-oats -timeout 5m spring-declarative-configuration/oats/
+# renovate: datasource=github-releases depName=gcx packageName=grafana/gcx
+export GCX_VERSION=v0.4.0
+go install "github.com/grafana/gcx/cmd/gcx@${GCX_VERSION}"
+
+workdir="$(mktemp -d)"
+trap 'rm -rf "$workdir"' EXIT
+git clone --depth 1 --branch v2 https://github.com/grafana/oats "$workdir/oats-src"
+GOWORK=off go -C "$workdir/oats-src" build -o "$workdir/oats" ./cmd/v2
+
+REAL_GCX_BIN="$(go env GOPATH)/bin/gcx" \
+  "$workdir/oats" \
+  --config oats.toml \
+  --gcx ./ci/oats/gcx-wrapper.sh \
+  --no-cache \
+  --timeout=10m
