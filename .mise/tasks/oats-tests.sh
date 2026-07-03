486e94fe69d577763a96dfba13cf5fc65fb758bc
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2026-07-03 11:54:58 +0200

    test: pin OATS via mise go backend Signed-off-by: Gregor Zeitlinger <gregor.zeitlinger@grafana.com>

--- .mise/tasks/oats-tests.sh
+++ .mise/tasks/oats-tests.sh
@@ -10,17 +10,8 @@ set -euo pipefail
   :opentelemetry-examples-logging-k8s-stdout-otlp-json:assemble \
   :opentelemetry-examples-spring-declarative-configuration:bootJar
 
-# renovate: datasource=github-releases depName=gcx packageName=grafana/gcx
-export GCX_VERSION=v0.4.0
-go install "github.com/grafana/gcx/cmd/gcx@${GCX_VERSION}"
-
-workdir="$(mktemp -d)"
-trap 'rm -rf "$workdir"' EXIT
-git clone --depth 1 --branch v2 https://github.com/grafana/oats "$workdir/oats-src"
-GOWORK=off go -C "$workdir/oats-src" build -o "$workdir/oats" ./cmd/v2
-
-REAL_GCX_BIN="$(go env GOPATH)/bin/gcx" \
-  "$workdir/oats" \
+REAL_GCX_BIN="$(command -v gcx)" \
+  oats \
   --config oats.toml \
   --gcx ./ci/oats/gcx-wrapper.sh \
   --no-cache \
